e755f5e82f6ba32a903237a197d465ba71e8946c
pooniavictus <poonia.taheri@gmail.com>
2025-01-20 20:38:56 +0330

    first

---
+++ src/main/java/com/mftplus/mftautomationsystemapplication/MftAutomationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mftplus.mftautomationsystemapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MftAutomationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MftAutomationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mftplus/mftautomationsystemapplication/MftAutomationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mftplus.mftautomationsystemapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MftAutomationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
