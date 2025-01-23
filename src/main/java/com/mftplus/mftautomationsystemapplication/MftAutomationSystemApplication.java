7ee6074b50a16e7e36241d6e0da98d7cc2aeb216
DESKTOP-VA5FTDD\Bahram <bahram.teymoori@outlook.com>
2025-01-23 20:32:14 +0330

    Add some annotations to main class

--- src/main/java/com/mftplus/mftautomationsystemapplication/MftAutomationSystemApplication.java
+++ src/main/java/com/mftplus/mftautomationsystemapplication/MftAutomationSystemApplication.java
@@ -2,8 +2,14 @@ package com.mftplus.mftautomationsystemapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableAspectJAutoProxy
+@EnableTransactionManagement
 public class MftAutomationSystemApplication {
 
     public static void main(String[] args) {
